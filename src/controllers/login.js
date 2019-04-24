40b800adb32e2f2132f565119ec1fb54b351b69c
Samantha Dickeson <samlucydickeson@gmail.com>
2019-04-24 11:14:02 +0100

    add github function

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,3 +1,26 @@
+const https = require("https");
+
+const getGitHub = url => {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, { headers: { "user-agent": "starsuit" } }, response => {
+        let allTheData = "";
+        response.on("data", chunk => {
+          allTheData += chunk;
+        });
+        response.on("end", () => {
+          // console.log(allTheData);
+          try {
+            resolve(JSON.parse(allTheData));
+          } catch (e) {
+            reject(`There was an error with the JSON: ${e}`);
+          }
+        });
+      })
+      .on("error", err => reject(`There was an error: ${err}`));
+  });
+};
+
 // renders login form on the page
 const get = (req, res) => {
   res.render("login");
@@ -7,9 +30,12 @@ const get = (req, res) => {
 // once "name" is added to the session variables in the browser, it's available throughout the app
 // so when we redirect to "/", req.session.name is still available
 const post = (req, res) => {
-  req.session.name = req.body.name;
-  req.session.image = req.body.image;
-  res.redirect("/");
+  const username = req.body.name;
+  getGitHub(`https://api.github.com/users/${username}`).then(response => {
+    req.session.image = response.avatar_url;
+    req.session.name = response.login;
+    res.redirect("/");
+  });
 };
 
 module.exports = { get, post };
